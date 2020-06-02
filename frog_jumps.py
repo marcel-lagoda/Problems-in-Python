293d18aa8b9f5e9fd8db5364593c660ad966e9e1
marcellagoda <marceli.lagoda@gmail.com>
2020-06-02 22:33:16 +0200

    Add frog_jumps.py #4

---
+++ frog_jumps.py
@@ -0,0 +1,12 @@
+# A frog jumps #4
+
+# A frog wants to cross the road.
+# The frog is currently located at position X
+# and wants to get to a position greater than or equal to Y.
+# The small frog always jumps a fixed distance, D.
+
+
+def solution(X, Y, D):
+    distance = Y - X
+    if distance % D == 0:
+        return distance // D
